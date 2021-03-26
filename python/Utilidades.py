b0eb081b4c75b9157979c2775c92401c42b0ea93
Renato Perseguini <48479261+rsiw2000@users.noreply.github.com>
2021-03-26 18:59:45 -0300

    removendo estáticos e enfatizando o conceito de herança

--- python/Utilidades.py
+++ python/Utilidades.py
@@ -11,15 +11,13 @@ from datetime import datetime
 
 class Util(object):
     # gera uma saudação, bom dia, tarde ou noite
-    @staticmethod
-    def saudacao(first_up=False):
-        iHora = int(datetime.now().strftime("%H"))
+    def saudacao(self, first_up=False):
+        iHora = self.safe_cast(int, datetime.now().strftime("%H"), 0)
         qual = "om dia" if iHora < 12 else "oa tarde" if iHora < 18 else "oa noite"
         return "%s%s" % (("B" if first_up else "b"), qual)
              
     # faz um cast com default, sem gerar erro
-    @staticmethod
-    def safe_cast(tipo, valor, default=None):
+    def safe_cast(self, tipo, valor, default=None):
         try:
             return tipo(valor)
         except:
@@ -27,18 +25,25 @@ class Util(object):
         return default
 
 # rotinas de teste
+# Vamos definir a classe de teste dentro da função, para que ela exista somente se a função for chamada
 def testes(args):
-    try:
-        print ("args", '->', args)
-        print ("Testes da classe Util")
-        nome = input("Digite seu nome: ")
-        print ("%s, %s." % (Util.saudacao(True), nome ))
-        idade = input("Qual sua idade: ")
-        print ("Ok, entao vc tem %d anos." % (Util.safe_cast(int, idade, 99)))
-    except Exception as err:
-        print ('Erro no teste - %s' % err)
-        sys.exit(1)
-    sys.exit(0)
+    # crio uma classe para o processo, que herda as funções da classe Util
+    class TesteUtil(Util):
+        def executa(self):
+            try:
+                print ("args", '->', args)
+                print ("Testes da classe Util")
+                nome = input("Digite seu nome: ")
+                print ("%s, %s." % (self.saudacao(True), nome ))
+                idade = input("Qual sua idade: ")
+                print ("Ok, então vc tem %d anos." % (self.safe_cast(int, idade, 99)))
+            except Exception as err:
+                print ('Erro no teste - %s' % err)
+                
+    # então aqui eu vou instanciar e executar o meu processo
+    proc = TesteUtil()
+    proc.executa()
+               
 
 # o nome é __main__ sempre que for executado diretamente (ex.  python Utilidades.py)
 # é bom para testes e não interfere quando este módulo for importado a partir de outro
